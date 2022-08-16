1704fad7ec37d8471018589bd06c6f2d8e19e35e
Shauryam Saxena <saxena.shauryam@gmail.com>
2022-08-16 15:53:07 +0530

    OTP verification added

--- app/src/main/java/com/code/pragati/LoginLayman.kt
+++ app/src/main/java/com/code/pragati/LoginLayman.kt
@@ -1,10 +1,17 @@
 package com.code.pragati
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.cardview.widget.CardView
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import java.util.concurrent.TimeUnit
 
 class LoginLayman : AppCompatActivity() {
 
@@ -13,6 +20,9 @@ class LoginLayman : AppCompatActivity() {
     private lateinit var google : CardView
     private lateinit var linkedIn : CardView
     private lateinit var social : CardView
+
+    private lateinit var auth: FirebaseAuth
+    private val TAG = "check"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +34,69 @@ class LoginLayman : AppCompatActivity() {
         linkedIn = findViewById(R.id.cardLoginWithLinkedIn)
         social = findViewById(R.id.cardLoginWithSocial)
 
+        auth = FirebaseAuth.getInstance()
+
+
+
+
+        getOTP.setOnClickListener {
+            val phoneNumber = number.text.toString().trim()
+            if (phoneNumber.isNotEmpty()) {
+                if (phoneNumber.length == 10) {
+
+                    //ProgressBar enable. Button Disable
+
+                    val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                            // This callback will be invoked in two situations:
+                            // 1 - Instant verification. In some cases the phone number can be instantly
+                            //     verified without needing to send or enter a verification code.
+                            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                            //     detect the incoming verification SMS and perform verification without
+                            //     user action.
+                            Log.d(TAG, "onVerificationCompleted:$credential")
+
+                        }
+
+                        override fun onVerificationFailed(e: FirebaseException) {
+                            // This callback is invoked in an invalid request for verification is made,
+                            // for instance if the the phone number format is not valid.
+                            Log.w(TAG, "onVerificationFailed", e)
+
+                            //Update e.message later
+                            Toast.makeText(this@LoginLayman,e.message,Toast.LENGTH_SHORT).show()
+                        }
+
+                        override fun onCodeSent(
+                            verificationId: String,
+                            token: PhoneAuthProvider.ForceResendingToken
+                        ) {
+                            // The SMS verification code has been sent to the provided phone number, we
+                            // now need to ask the user to enter the code and then construct a credential
+                            // by combining the code with a verification ID.
+                            Log.d(TAG, "onCodeSent:$verificationId")
+
+                            val intent = Intent(this@LoginLayman, OTPVerification::class.java)
+                            intent.putExtra("otp",verificationId)
+                            startActivity(intent)
+                        }
+                    }
+
+                    val options = PhoneAuthOptions.newBuilder(auth)
+                        .setPhoneNumber("+91$phoneNumber")       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+                        .build()
+                    PhoneAuthProvider.verifyPhoneNumber(options)
+
+                } else {
+                    Toast.makeText(this,"Please enter correct mobile number",Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this,"Enter mobile number",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
