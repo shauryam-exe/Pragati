b73d03a0e09d660ccfde281c3e91c9b28ab7296b
IamBansal <abansal11ae@gmail.com>
2022-08-26 02:58:02 +0530

    Video fetched

--- app/src/main/java/com/code/pragati/fragments/PitchFragment.kt
+++ app/src/main/java/com/code/pragati/fragments/PitchFragment.kt
@@ -1,19 +1,22 @@
 package com.code.pragati.fragments
 
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.code.pragati.R
+import com.code.pragati.adapters.VideoAdapter
+import com.code.pragati.model.PS
+import com.code.pragati.model.Video
+import com.code.pragati.model.VideoD
 import com.code.pragati.model.VideoItem
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import androidx.viewpager2.widget.ViewPager2
-import com.code.pragati.R
-import com.code.pragati.adapters.VideoAdapter
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -42,12 +45,6 @@ class PitchFragment : Fragment() {
         return layout
     }
 
-    private fun showVideo(videoItems : ArrayList<VideoItem>){
-
-
-    }
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -55,51 +52,28 @@ class PitchFragment : Fragment() {
 
         val videoItems = arrayListOf<VideoItem>()
 
-
-
         FirebaseDatabase.getInstance().reference.child("Video")
             .addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
-//                    for (dataSnaps in snapshot.children){
-//                        val video = snapshot.getValue(Video::class.java)
-//                        val url = video?.uri.toString()
-//                        Toast.makeText(context, video?.name, Toast.LENGTH_SHORT).show()
-//                        val videoItem = VideoItem(url, "nothing", "ahvckajc", "student")
-//                        videoItems.add(videoItem)
-//                    }
+                    for (dataSnap in snapshot.children){
+                        val video: VideoD? = dataSnap.getValue(VideoD::class.java)
+                        videoItems.add(VideoItem(video!!.uri, "skjbdcklscd", "sccklnscs", "cgkjswcw"))
+                    }
                 }
 
                 override fun onCancelled(error: DatabaseError) {
                     TODO("Not yet implemented")
                 }
 
+            })
 
-            }
-            )
 
-        val videoItem4 = VideoItem(
-            url = "https://firebasestorage.googleapis.com/v0/b/pragati-a904c.appspot.com/o/videos%2F1661455444724.mp4?alt=media&token=fa5b09bd-3ff3-4777-bc75-e9b48bbd84ed",
-            ideaName = "Sugar Cosmetics",
-            founderName = "Jenifer",
-            "student")
-        videoItems.add(videoItem4)
-        val videoItem5 = VideoItem(
-            url = "https://firebasestorage.googleapis.com/v0/b/pragati-a904c.appspot.com/o/videos%2F1661455444724.mp4?alt=media&token=fa5b09bd-3ff3-4777-bc75-e9b48bbd84ed",
-            ideaName = "Sugar Cosmetics",
-            founderName = "Jenifer",
-            "student")
-        videoItems.add(videoItem5)
-        val videoItem3 = VideoItem(
-            url = "https://firebasestorage.googleapis.com/v0/b/pragati-a904c.appspot.com/o/videos%2F1661455444724.mp4?alt=media&token=fa5b09bd-3ff3-4777-bc75-e9b48bbd84ed",
-            ideaName = "Sugar Cosmetics",
-            founderName = "Jenifer",
-            "student")
-        videoItems.add(videoItem3)
         val videoItem2 = VideoItem(
-            url = "https://firebasestorage.googleapis.com/v0/b/pragati-a904c.appspot.com/o/videos%2F1661455444724.mp4?alt=media&token=fa5b09bd-3ff3-4777-bc75-e9b48bbd84ed",
+            url = "https://firebasestorage.googleapis.com/v0/b/pragati-a904c.appspot.com/o/videos%2F1661455797702.mp4?alt=media&token=3218b71c-aac0-41df-8d2f-87f1c351f699",
             ideaName = "Sugar Cosmetics",
             founderName = "Jenifer",
-            "student")
+            "student"
+        )
         videoItems.add(videoItem2)
 
         videoViewPager.adapter = VideoAdapter(videoItems)
