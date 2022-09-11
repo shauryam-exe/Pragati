ded0e783d4e2f9846d82b93d25009e1efd010321
IamBansal <abansal11ae@gmail.com>
2022-09-11 14:32:26 +0530

    Edit profile Added

--- app/src/main/java/com/code/pragati/EditProfile.kt
+++ app/src/main/java/com/code/pragati/EditProfile.kt
@@ -6,9 +6,11 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.cardview.widget.CardView
 import androidx.core.widget.doOnTextChanged
 import com.code.pragati.model.User
 import com.google.firebase.auth.FirebaseAuth
@@ -16,12 +18,21 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import de.hdodenhof.circleimageview.CircleImageView
 
 class EditProfile : AppCompatActivity() {
 
     private lateinit var back : ImageView
     private lateinit var enterPass : EditText
+    private lateinit var usernameEdit : EditText
+    private lateinit var emailEdit : EditText
+    private lateinit var newPassEdit : EditText
+    private lateinit var usernameCard : CardView
+    private lateinit var emailCard : CardView
+    private lateinit var newPassCard : CardView
     private lateinit var correctPass : ImageView
+    private lateinit var profileImage : CircleImageView
+    private lateinit var editImage : ImageButton
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var saveBtn : AppCompatButton
 
@@ -33,13 +44,19 @@ class EditProfile : AppCompatActivity() {
         enterPass = findViewById(R.id.etEnterCurrentPass)
         correctPass = findViewById(R.id.ivCorrectPass)
         saveBtn = findViewById(R.id.btnSaveNewDetails)
+        usernameEdit = findViewById(R.id.etUsernameEdit)
+        emailEdit = findViewById(R.id.etEmailEdit)
+        newPassEdit = findViewById(R.id.etNewPassEdit)
+        usernameCard = findViewById(R.id.cardUsername)
+        emailCard = findViewById(R.id.cardEmailEdit)
+        newPassCard = findViewById(R.id.cardNewPassEdit)
+        profileImage = findViewById(R.id.civProfile)
+        editImage = findViewById(R.id.ibChangeProfile)
         firebaseAuth = FirebaseAuth.getInstance()
 
         back.setOnClickListener {
             onBackPressed()
         }
-
-        correctPass.visibility = View.INVISIBLE
 
         val map = HashMap<String, String>()
 
@@ -61,13 +78,9 @@ class EditProfile : AppCompatActivity() {
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 if (p0.contentEquals(map["pass"], false)) {
-                    correctPass.visibility = View.VISIBLE
-                    saveBtn.isEnabled = true
-                    saveBtn.alpha = 1f
+                    enableFeatures()
                 } else {
-                    correctPass.visibility = View.INVISIBLE
-                    saveBtn.isEnabled = false
-                    saveBtn.alpha = 0.3f
+                    disableFeatures()
                 }
             }
 
@@ -75,4 +88,33 @@ class EditProfile : AppCompatActivity() {
         })
 
     }
+
+    private fun enableFeatures(){
+        correctPass.visibility = View.VISIBLE
+        saveBtn.isEnabled = true
+        usernameEdit.isEnabled = true
+        emailEdit.isEnabled = true
+        newPassEdit.isEnabled = true
+        saveBtn.alpha = 1f
+        profileImage.alpha = 1f
+        editImage.alpha = 1f
+        usernameCard.alpha = 1f
+        emailCard.alpha = 1f
+        newPassCard.alpha = 1f
+    }
+
+    private fun disableFeatures(){
+        correctPass.visibility = View.INVISIBLE
+        saveBtn.isEnabled = false
+        usernameEdit.isEnabled = false
+        emailEdit.isEnabled = false
+        newPassEdit.isEnabled = false
+        saveBtn.alpha = 0.3f
+        profileImage.alpha = 0.4f
+        editImage.alpha = 0.4f
+        usernameCard.alpha = 0.4f
+        emailCard.alpha = 0.4f
+        newPassCard.alpha = 0.4f
+    }
+
 }
--- app/src/main/java/com/code/pragati/ui/login/ForgotPass.kt
+++ app/src/main/java/com/code/pragati/ui/login/ForgotPass.kt
@@ -1,14 +1,16 @@
 package com.code.pragati.ui.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.telephony.SmsManager
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.widget.*
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.cardview.widget.CardView
 import com.code.pragati.R
 
 class ForgotPass : AppCompatActivity() {
@@ -19,6 +21,8 @@ class ForgotPass : AppCompatActivity() {
     private lateinit var enterOtp : EditText
     private lateinit var newPass : EditText
     private lateinit var confirmPass : EditText
+    private lateinit var newPassCard : CardView
+    private lateinit var confirmPassCard : CardView
     private lateinit var continueLogin : AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,8 +36,9 @@ class ForgotPass : AppCompatActivity() {
         newPass = findViewById(R.id.etNewPassForgot)
         confirmPass = findViewById(R.id.etConfirmPassForgot)
         continueLogin = findViewById(R.id.btnContinueLoginForgot)
+        newPassCard = findViewById(R.id.cardNewPassForgot)
+        confirmPassCard = findViewById(R.id.cardConfirmPassForgot)
 
-        correctOtp.visibility = View.INVISIBLE
         incorrectOtp.visibility = View.INVISIBLE
 
         back.setOnClickListener{
@@ -44,39 +49,47 @@ class ForgotPass : AppCompatActivity() {
             startActivity(Intent(this, LoginOthers::class.java))
         }
 
-//        val otp = intent.getStringExtra("otp")
-//        verifyOtp(otp!!)
-//        Toast.makeText(this, "OTP after intent is : $otp", Toast.LENGTH_LONG).show()
+        val otp = intent.getStringExtra("otp")
+        verifyOtp(otp!!)
 
     }
 
     private fun verifyOtp(otp : String) {
-
         enterOtp.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                TODO("Not yet implemented")
-            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                TODO("Not yet implemented")
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-                if(p0?.length == 6){
-                    if (p0.equals(otp)){
-                        newPass.isEnabled = true
-                        newPass.alpha = 1f
-                        confirmPass.alpha = 1f
-                        continueLogin.alpha = 1f
-                        confirmPass.isEnabled = true
-                        continueLogin.isEnabled = true
-                        correctOtp.visibility = View.VISIBLE
-                    } else {
-                        incorrectOtp.visibility = View.VISIBLE
-                    }
+                if (p0.contentEquals(otp)) {
+                    enableFeatures()
+                } else if (p0!!.length == 6 && !p0.contentEquals(otp)) {
+                    incorrectOtp.visibility = View.VISIBLE
+                } else {
+                    disableFeatures()
                 }
             }
+            override fun afterTextChanged(p0: Editable?) {}
         })
-
     }
+
+    private fun enableFeatures(){
+        correctOtp.visibility = View.VISIBLE
+        incorrectOtp.visibility = View.INVISIBLE
+        continueLogin.isEnabled = true
+        newPass.isEnabled = true
+        confirmPass.isEnabled = true
+        continueLogin.alpha = 1f
+        newPassCard.alpha = 1f
+        confirmPassCard.alpha = 1f
+    }
+
+    private fun disableFeatures(){
+        correctOtp.visibility = View.INVISIBLE
+        continueLogin.isEnabled = false
+        newPass.isEnabled = false
+        confirmPass.isEnabled = false
+        continueLogin.alpha = 0.3f
+        newPassCard.alpha = 0.4f
+        confirmPassCard.alpha = 0.4f
+    }
+
 }
